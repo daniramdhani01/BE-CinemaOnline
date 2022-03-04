8d7ea052c357b2351919183a5214de83dc3f9515
Dani Ramdhani <dani.ramdhani38@gmail.com>
2022-03-04 18:59:35 +0700

    update

--- index.js
+++ index.js
@@ -10,6 +10,6 @@ app.use(cors())
 app.use('/api/v1/', router);
 app.use('/uploads', express.static('uploads'))
 
-const port = 5000
+const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
